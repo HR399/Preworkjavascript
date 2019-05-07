7f5899d089e36d5dfc04a9ad037e6d173c89f5b0
ianstump <ianstump@gmail.com>
2019-05-07 13:51:03 +0200

    Update EX14Return Largest Numbers in arrays.js

--- EX14Return Largest Numbers in arrays.js
+++ EX14Return Largest Numbers in arrays.js
@@ -10,12 +10,19 @@ largest_of_four([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 
 function largest_of_four(arr){
     // step 1. creat an array that will host the result of the 4-subarrays
     var largestNumber=[0,0,0,0]
+    
+        // you can declare the array like this largestNumber=[]
+    
     //step 2. creat the first FOR loop that will iterate through the arrays
     for(var arrayIndex = 0; arrayIndex < arr.length; arrayIndex++) {
         //the starting point, index 0 ===the firs array
 
         //step 3. create the second for loop that will iterate through the nested array
         for(var subArrayIndex = 0; subArrayIndex<arr[arrayIndex].length; subArrayIndex++ ){
+                                    // ^ I dont get what you did here
+            
+     // you are now inside each array, my advice is to investigate an easier way of getting the biggest number inside an array.
+            
             // the starting point, index 0 === first subarry
         }
         if(arr[arrayIndex][subArrayIndex] > largestNumber[arrayIndex]){
